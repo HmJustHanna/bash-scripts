d379708c33de050c065a5e795bde05c023886379
HmJustHanna <evdokimovaaa.job@gmail.com>
2023-11-27 23:27:27 +0300

    finish: bash script which create default c project for me -> make-project.sh

--- make-project.sh
+++ make-project.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+mkdir $1
+cd $1
 mkdir obj src && touch Makefile
-echo'CC = gcc
+echo 'CC = gcc
 TARGET = program
 PREF_SRC = ./src/
 PREF_OBJ = ./obj/
@@ -17,12 +18,23 @@ $(PREF_OBJ)%.o: $(PREF_SRC)%.c
 	$(CC) $(CFLAGS) -c $< -o $@
 clean:
 	rm -rf $(TARGET) $(PREF_OBJ)*.o' > Makefile
+	
 CURRENT_DATE=$(date)
-CREATOR=$(USER)
-touch README.txt && echo 'Hi! :D I\'m $CREATOR and I\'m learning the c programming language
-project name: ...
-date: $CURRENT_DATE
-goal: ...' > README.txt
+CREATOR="$USER"
+touch README.txt
+echo "Hi, I'm $CREATOR and I'm learning the c programming language :D 
+
+PROJECT:
+
+NAME: $1
+TASKS:
+*
+*
+*
+------------------------------
+START: $CURRENT_DATE
+END:" > README.txt
+
 cd src && touch main.c
 echo '#include <stdio.h>
 int main() {
