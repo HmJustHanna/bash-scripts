1b3d324e8e2130aea1ab9525ca9a63767873f330
HmJustHanna <evdokimovaaa.job@gmail.com>
2023-12-31 00:24:55 +0300

    add beautiful lockscreen script with i3lock-color

---
+++ backup/backup-configs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo cp -r $HOME/.config/* /media/dbackup/configs
+sudo cp $HOME/.bashrc /media/dbackup/configs
+sudo cp $HOME/.vimrc /media/dbackup/configs
+sudo cp $HOME/.gitconfig /media/dbackup/configs
+
--- install-fire-fox.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo '
-Package: *
-Pin: release o=LP-PPA-mozillateam
-Pin-Priority: 1001
-' | sudo tee /etc/apt/preferences.d/mozilla-firefox
