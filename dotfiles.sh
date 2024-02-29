45fe18d8c13b7f11f72dff852d2c871b04cc0d6d
HmJustHanna <evdokimovaaa.job@gmail.com>
2024-03-01 01:10:33 +0300

    update

--- dotfiles.sh
+++ dotfiles.sh
@@ -9,11 +9,12 @@ cp $HOME/.xinitrc $HOME/dotfiles/.xinitrc
 cp $HOME/.config/zathura/zathurarc $HOME/dotfiles/.zathurarc
 cp $HOME/.config/htop/htoprc $HOME/dotfiles/.htoprc
 
-cp $HOME/.config/cava/config $HOME//dotfiles/cava/config
+cp $HOME/.config/cava/config $HOME/dotfiles/cava/config
 cp $HOME/.moc/config $HOME/dotfiles/moc/config
 cp $HOME/.moc/keymap $HOME/dotfiles/moc/keymap
 
 cp $HOME/.gitconfig $HOME/dotfiles/.gitconfig
+cp /etc/pacman.conf $HOME/dotfiles/pacman.conf
 
 
 
