f4b43134505434bbb96f9389fe9c078407cb7a7f
HmJustHanna <evdokimovaaa.job@gmail.com>
2024-03-13 11:08:45 +0300

    update

--- scrot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mkdir -p /home/anna/Pictures/scrot && scrot /home/anna/Pictures/scrot/'%d%m%Y-%H%M%S-$wx$h.png'
--- scrotsel.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-scrot /home/anna/Media/Pictures/scrot/'%d%m%Y-%H%M%S-$wx$h.png' --select --line mode=edge
