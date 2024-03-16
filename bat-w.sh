9fedff81c195e95f75e3bb599ae5ed3384de7f56
HmJustHanna <evdokimovaaa.job@gmail.com>
2024-03-16 13:48:57 +0300

    add dmenu-translate script

--- bat-w.sh
+++ bat-w.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-BAT=$(cat /sys/class/power_supply/BAT1/capacity)                                #battery percentage
+BAT=$(cat /sys/class/power_supply/BAT1/capacity)
+#sudo -u anna DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 
-if [ $BAT -eq '94' ]; then
-    notify-send -w 'Warning!' 'BAT: 94%'
-else
-    echo "..."
+if [ $BAT -eq '86' ]; then
+    notify-send -w -u critical 'Warning!' 'BAT: 86%'
 fi
+
+
 
