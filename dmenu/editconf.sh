f45e4b9bfa4427962cfa027450910eb32f31d2f9
HmJustHanna <evdokimovaaa.job@gmail.com>
2024-03-19 23:28:50 +0300

    update

--- dmenu/editconf.sh
+++ dmenu/editconf.sh
@@ -14,7 +14,7 @@ choice=$(printf '%s\n' "${list[@]}" | dmenu -i -l 10 -p 'edit:')
 
 if [ "$choice" ]; then
     config=$(printf '%s\n' "${choice}" | awk '{print $NF}')
-    $EDITOR "$config"
+    urxvt -e vim "$config"
 else
     echo "exit" && exit 1
 fi
